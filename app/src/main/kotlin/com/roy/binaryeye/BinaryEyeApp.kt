25978a71d19c7b76245a1e48f2ea61249a1bb5a2
roy <roy93group@gmail.com>
2023-08-06 14:08:15 +0700

    update

--- app/src/main/kotlin/com/roy/binaryeye/BinaryEyeApp.kt
+++ app/src/main/kotlin/com/roy/binaryeye/BinaryEyeApp.kt
@@ -4,6 +4,23 @@ import android.app.Application
 import com.roy.binaryeye.db.Db
 import com.roy.binaryeye.preference.Preferences
 
+
+//TODO firebase
+//TODO animation
+//TODO rate app
+//TODO more app
+//TODO share app
+//TODO policy
+//TODO proguard
+//TODO app version
+//TODO leak canary
+//TODO keystore
+//TODO ad id
+//TODO ad applovin
+
+//done
+//ic_launcher
+
 val db = Db()
 val prefs = Preferences()
 
