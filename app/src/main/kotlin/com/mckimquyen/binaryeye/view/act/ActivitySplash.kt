f1d287279a0b57ec712590c09f95d02e23ff3c0e
roy <loitp@galaxyjoy.vn>
2024-12-14 14:39:18 +0700

    update

--- app/src/main/kotlin/com/mckimquyen/binaryeye/view/act/ActivitySplash.kt
+++ app/src/main/kotlin/com/mckimquyen/binaryeye/view/act/ActivitySplash.kt
@@ -3,6 +3,8 @@ package com.mckimquyen.binaryeye.view.act
 import android.content.Intent
 import android.os.Bundle
 import com.mckimquyen.binaryeye.BaseActivity
+import com.mckimquyen.binaryeye.BuildConfig
+import com.mckimquyen.binaryeye.ext.rateAppInApp
 
 class ActivitySplash : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,11 @@ class ActivitySplash : BaseActivity() {
 
         startActivity(Intent(applicationContext, CameraActivity::class.java))
         finish()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        rateAppInApp(BuildConfig.DEBUG)
     }
 }
 
