ff199455ae1eedb49d0f3bd3087eb629c2fc268e
roy <loitp@galaxyjoy.vn>
2024-12-22 14:30:39 +0700

    update

--- app/src/main/kotlin/com/mckimquyen/binaryeye/view/act/ActivitySplash.kt
+++ app/src/main/kotlin/com/mckimquyen/binaryeye/view/act/ActivitySplash.kt
@@ -2,18 +2,30 @@ package com.mckimquyen.binaryeye.view.act
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.mckimquyen.binaryeye.BaseActivity
+import com.mckimquyen.binaryeye.R
 
 class ActivitySplash : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.roy_a_splash)
         // It's important _not_ to inflate a layout file here
         // because that would happen after the app is fully
         // initialized what is too late.
 
-        startActivity(Intent(applicationContext, CameraActivity::class.java))
-        finish()
+        setupViews()
+    }
+
+    fun setupViews() {
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(applicationContext, CameraActivity::class.java))
+                finish()
+            },
+            1000
+        )
     }
 }
 
